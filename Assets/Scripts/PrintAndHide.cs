79fdc2011e1899cc3a5ac0f31ffa47dd978e0419
David Li <37282826+davidli731@users.noreply.github.com>
2022-08-22 15:01:32 +1000

    Distinction 90% Commit

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrintAndHide : MonoBehaviour
+public class ConsolePrint : MonoBehaviour
 {
 
     private int i;
-    public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
